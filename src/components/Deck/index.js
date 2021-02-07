dda80fda163b6867d2e6e4fe82a41b20c6615247
Yong Jheng Tang <tyj@yongs-mbp.home>
2021-02-07 23:23:14 +0000

    add history

--- src/components/Deck/index.js
+++ src/components/Deck/index.js
@@ -3,53 +3,30 @@ import CardBack from 'components/Card/CardBack';
 import CardFace from 'components/Card/CardFace';
 import AttributeBadge from 'components/AttributeBadge';
 
-const Deck = ({ cards, selectCard, title, invisible }) => {
+const Deck = ({ cards, selectCard, label, invisible }) => {
   const competeAttr = useSelector((state) => state.diceOutcome.label);
   const isPhaseStarted = useSelector((state) => state.isPhaseStarted);
   const selectedCardSet = useSelector((state) => state.selectedCardSet);
 
-  const renderCardInfo = (card) => {
-    switch (competeAttr) {
-      case 'people': {
-        return (
-          <AttributeBadge
-            label="piloted starships "
-            value={card.starships?.length}
-          />
-        );
-      }
-
-      case 'starships': {
-        return (
-          <AttributeBadge
-            label="max speed "
-            value={card.max_atmosphering_speed}
-          />
-        );
-      }
-
-      case 'vehicles': {
-        return <AttributeBadge label="capacity " value={card.cargo_capacity} />;
-      }
-    }
-  };
   return (
-    <div className="mx-auto w-80" style={{ maxWidth: '600px' }}>
-      <span>{title}</span>
-      <div className="card-deck">
-        {cards.map((card, i) => {
-          return invisible ? (
-            <CardBack key={i} />
-          ) : (
-            <CardFace
-              key={i}
-              selectCard={() => selectCard(i)}
-              disable={!isPhaseStarted}
-              cardType={competeAttr}
-              card={card}
-            />
-          );
-        })}
+    <div className="d-flex flex-row-reverse justify-content-center">
+      <div style={{ maxWidth: '600px' }}>
+        <h4>{label}</h4>
+        <div className="card-deck">
+          {cards.map((card, i) => {
+            return invisible ? (
+              <CardBack key={i} />
+            ) : (
+              <CardFace
+                key={i}
+                selectCard={() => selectCard(i)}
+                disable={!isPhaseStarted}
+                cardType={competeAttr}
+                card={card}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -11,6 +11,7 @@ const Game = () => {
   const isPhaseStarted = useSelector((state) => state.isPhaseStarted);
   const diceOutcome = useSelector((state) => state.diceOutcome);
   const cardDecks = useSelector((state) => state.cardDecks);
+  const selectedCardSet = useSelector((state) => state.selectedCardSet);
   const scoreOfPlayer = useSelector((state) => state.scoreOfPlayer);
   const scoreOfComputer = useSelector((state) => state.scoreOfComputer);
 
@@ -24,34 +25,49 @@ const Game = () => {
     }
   };
 
+  console.log(selectedCardSet);
+
   return (
-    <div className="vh-100 d-flex flex-column text-center">
+    <div>
       {/* <h1>Winner: {calculateWinner(scoreOfPlayer, scoreOfComputer)}</h1> */}
-
-      <div className="flex-grow-1">
-        <Deck
-          title={`Computer : ${scoreOfComputer}`}
-          cards={cardDecks.cardsOfComputer}
-          selectCard={onClickCard(cardDecks, diceOutcome.value)}
-          invisible
-        />
+      <div className="text-white bg-dark" style={{ height: '60px' }}>
+        <p style={{ lineHeight: '60px' }}>
+          {selectedCardSet
+            ? `HISTORY ${selectedCardSet.selectedCardOfPlayer.name} vs ${selectedCardSet.selectedCardOfComputer.name}`
+            : 'War between the bright and the dark begins. More power you own, more attack you underwent.'}
+        </p>
       </div>
+      <div
+        className="d-flex flex-column text-center m-4"
+        style={{ height: '90vh' }}
+      >
+        <div className="flex-grow-1">
+          <Deck
+            label={`COMPUTER ${scoreOfComputer}`}
+            value={scoreOfComputer}
+            cards={cardDecks.cardsOfComputer}
+            selectCard={onClickCard(cardDecks, diceOutcome.value)}
+            invisible
+          />
+        </div>
 
-      <div className="flex-grow-1">
-        <Dice
-          diceLabel={diceOutcome.label}
-          isPhaseStarted={isPhaseStarted}
-          handleClickDice={() => dispatch(distributeCards())}
-        />
-      </div>
+        <div className="flex-grow-1">
+          <Dice
+            diceLabel={diceOutcome.label}
+            isPhaseStarted={isPhaseStarted}
+            handleClickDice={() => dispatch(distributeCards())}
+          />
+        </div>
 
-      <div className="flex-grow-1">
-        <Deck
-          title={`Player : ${scoreOfPlayer}`}
-          cards={cardDecks.cardsOfPlayer}
-          selectCard={onClickCard(cardDecks, diceOutcome.value)}
-          invisible={!isPhaseStarted}
-        />
+        <div className="flex-grow-1">
+          <Deck
+            label={`PLAYER ${scoreOfPlayer}`}
+            value={scoreOfPlayer}
+            cards={cardDecks.cardsOfPlayer}
+            selectCard={onClickCard(cardDecks, diceOutcome.value)}
+            invisible={!isPhaseStarted}
+          />
+        </div>
       </div>
     </div>
   );
