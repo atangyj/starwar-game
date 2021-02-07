13f492321300d742d802ed1c4e6473af19bbc8e6
Yong Jheng Tang <tyj@yongs-mbp.home>
2021-02-07 17:34:13 +0000

    create spinning dice

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import './Dice.css';
+
 const Dice = ({ diceLabel, handleClickDice, isPhaseStarted }) => {
+  const [animate, setAnimate] = useState(false);
+  const tossDice = () => {
+    setAnimate(true);
+    setTimeout(handleClickDice, 1000);
+  };
   return (
-    <button type="button" onClick={handleClickDice} disabled={isPhaseStarted}>
-      {isPhaseStarted ? `Compete with ${diceLabel}` : 'Toss dice'}
+    <button
+      type="button"
+      onClick={tossDice}
+      disabled={isPhaseStarted}
+      style={{ width: '100px', height: '100px' }}
+      className={`rounded-lg dice ${animate && 'spin'}`}
+    >
+      {isPhaseStarted ? `${diceLabel}` : 'Toss Dice'}
     </button>
   );
 };
